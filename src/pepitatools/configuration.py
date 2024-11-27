3f85ff04ae13e9ec10c5cb78375a4d2704b26fc9
Braden Griebel <bgriebel@uw.edu>
2024-11-27 13:53:54 -0800

    Updated Config File Handling

--- src/pepitatools/configuration.py
+++ src/pepitatools/configuration.py
@@ -35,7 +35,7 @@ _CONFIGURATION = {
     "filename_replacement_subtr_infection": "CH2|CH1",
     "filename_replacement_subtr_ototox": "CH1|CH2",
     "log_dir": "/path/to/log/dir",
-    "keyence_file": None,
+    "keyence_lenses_file": None,
 }
 
 _DEFAULT_CONFIG_STR = """
@@ -71,62 +71,72 @@ def set_config_setting(setting, value):
 def read_config(config_file: Union[str, os.PathLike, None] = None) -> None:
     if config_file is None:
         config_file = pathlib.Path(".") / "config.ini"
+    else:
+        config_file = pathlib.Path(config_file)
+    # If the config file isn't created, keep going to try and use config options passed in from the
+    # command line
+    if not config_file.exists():
+        return None
 
     parsed_config = configparser.ConfigParser()
     parsed_config.read(str(config_file))
 
-    _CONFIGURATION["absolute_max_infection"] = parsed_config[_CONFIG_SECTION][
-        "absolute_max_infection"
-    ]
-    _CONFIGURATION["absolute_min_infection"] = parsed_config[_CONFIG_SECTION][
-        "absolute_min_infection"
-    ]
-    _CONFIGURATION["absolute_max_ototox"] = parsed_config[_CONFIG_SECTION][
-        "absolute_max_ototox"
-    ]
-    _CONFIGURATION["absolute_min_ototox"] = parsed_config[_CONFIG_SECTION][
-        "absolute_min_ototox"
-    ]
-    _CONFIGURATION["channel_main_ototox"] = parsed_config[_CONFIG_SECTION][
-        "channel_main_ototox"
-    ]
-    _CONFIGURATION["channel_main_infection"] = parsed_config[_CONFIG_SECTION][
-        "channel_main_infection"
-    ]
-    _CONFIGURATION["channel_subtr_ototox"] = parsed_config[_CONFIG_SECTION][
-        "channel_subtr_ototox"
-    ]
-    _CONFIGURATION["channel_subtr_infection"] = parsed_config[_CONFIG_SECTION][
-        "channel_subtr_infection"
-    ]
-    _CONFIGURATION["filename_replacement_delimiter"] = parsed_config[_CONFIG_SECTION][
-        "filename_replacement_delimiter"
-    ]
-    _CONFIGURATION["filename_replacement_brightfield_infection"] = parsed_config[
-        _CONFIG_SECTION
-    ]["filename_replacement_brightfield_infection"]
-    _CONFIGURATION["filename_replacement_brightfield_ototox"] = parsed_config[
-        _CONFIG_SECTION
-    ]["filename_replacement_brightfield_ototox"]
-    _CONFIGURATION["filename_replacement_mask_infection"] = parsed_config[
-        _CONFIG_SECTION
-    ]["filename_replacement_mask_infection"]
-    _CONFIGURATION["filename_replacement_mask_ototox"] = parsed_config[_CONFIG_SECTION][
-        "filename_replacement_mask_ototox"
-    ]
-    _CONFIGURATION["filename_replacement_subtr_infection"] = parsed_config[
-        _CONFIG_SECTION
-    ]["filename_replacement_subtr_infection"]
-    _CONFIGURATION["filename_replacement_subtr_ototox"] = parsed_config[
-        _CONFIG_SECTION
-    ]["filename_replacement_subtr_ototox"]
-    _CONFIGURATION["log_dir"] = parsed_config[_CONFIG_SECTION]["log_dir"]
-    # Make sure the keyence_file isn't just the default, if it is set it to None for easier handling elsewhere
-    _CONFIGURATION["keyence_lenses_file"] = (
-        None
-        if (
-            parsed_config[_CONFIG_SECTION]["keyence_lenses_file"]
-            == "/path/to/keyence/file"
-        )
-        else parsed_config[_CONFIG_SECTION]["keyence_lenses_file"]
+    # Update the config, falling back on the default
+    global _CONFIGURATION
+    _CONFIGURATION["absolute_max_infection"] = parsed_config.get(
+        _CONFIG_SECTION, "absolute_max_infection", fallback=26249
     )
+    _CONFIGURATION["absolute_min_infection"] = parsed_config.get(
+        _CONFIG_SECTION, "absolute_min_infection", fallback=431
+    )
+    _CONFIGURATION["absolute_max_ototox"] = parsed_config.get(
+        _CONFIG_SECTION, "absolute_max_ototox", fallback=26249
+    )
+    _CONFIGURATION["absolute_min_ototox"] = parsed_config.get(
+        _CONFIG_SECTION, "absolute_min_ototox", fallback=431
+    )
+    _CONFIGURATION["channel_main_ototox"] = parsed_config.get(
+        _CONFIG_SECTION, "channel_main_ototox", fallback=1
+    )
+    _CONFIGURATION["channel_main_infection"] = parsed_config.get(
+        _CONFIG_SECTION, "channel_main_infection", fallback=0
+    )
+    _CONFIGURATION["channel_subtr_ototox"] = parsed_config.get(
+        _CONFIG_SECTION, "channel_subtr_ototox", fallback=0
+    )
+    _CONFIGURATION["channel_subtr_infection"] = parsed_config.get(
+        _CONFIG_SECTION, "channel_subtr_infection", fallback=1
+    )
+    _CONFIGURATION["filename_replacement_delimiter"] = parsed_config.get(
+        _CONFIG_SECTION, "filename_replacement_delimiter", fallback="|"
+    )
+    _CONFIGURATION["filename_replacement_brightfield_infection"] = parsed_config.get(
+        _CONFIG_SECTION,
+        "filename_replacement_brightfield_infection",
+        fallback="CH2|CH4",
+    )
+    _CONFIGURATION["filename_replacement_brightfield_ototox"] = parsed_config.get(
+        _CONFIG_SECTION, "filename_replacement_brightfield_ototox", fallback="CH1|CH4"
+    )
+    _CONFIGURATION["filename_replacement_mask_infection"] = parsed_config.get(
+        _CONFIG_SECTION, "filename_replacement_mask_infection", fallback="CH2|mask"
+    )
+    _CONFIGURATION["filename_replacement_mask_ototox"] = parsed_config.get(
+        _CONFIG_SECTION, "filename_replacement_mask_ototox", fallback="CH1|mask"
+    )
+    _CONFIGURATION["filename_replacement_subtr_infection"] = parsed_config.get(
+        _CONFIG_SECTION, "filename_replacement_subtr_infection", fallback="CH2|CH1"
+    )
+    _CONFIGURATION["filename_replacement_subtr_ototox"] = parsed_config.get(
+        _CONFIG_SECTION, "filename_replacement_subtr_ototox", fallback="CH1|CH2"
+    )
+    _CONFIGURATION["log_dir"] = parsed_config.get(
+        _CONFIG_SECTION, "log_dir", fallback="/path/to/log/dir"
+    )
+    _CONFIGURATION["keyence_lenses_file"] = parsed_config.get(
+        _CONFIG_SECTION, "keyence_file", fallback=None
+    )
+
+    # Make sure the keyence_file isn't just the default, if it is set it to None for easier handling elsewhere
+    if _CONFIGURATION["keyence_lenses_file"] == "/path/to/keyence/file":
+        _CONFIGURATION["keyence_lenses_file"] = None
